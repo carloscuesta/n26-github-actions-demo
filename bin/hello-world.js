8665ec3ad7200d03a63292de09d365b257c3f6bd
Carlos Cuesta <carloscuesta@me.com>
2022-03-16 16:42:50 +0100

    Create issue on push

--- bin/hello-world.js
+++ bin/hello-world.js
@@ -1,2 +1,21 @@
 #!/usr/bin/env node
-console.log('Hello world!')
+import { Octokit } from '@octokit/rest'
+
+(async() => {
+  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
+
+  try {
+    console.log('Trying to create the issue...')
+
+    await octokit.rest.issues.create({
+      owner: 'carloscuesta',
+      repo: 'n26-github-actions-demo',
+      title: 'Hello world!',
+      body: '## Hello world! 👋'
+    });
+
+    console.log('Success! ✅')
+  } catch (error) {
+    console.error('ERROR: Something happened', error)
+  }
+})()
